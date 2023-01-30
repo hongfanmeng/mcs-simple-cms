f857cc912aff6f1097fbe89370b9e91d4d172292
StevenH <hongfanmeng@gmail.com>
2023-01-30 17:41:30 +0800

    feat: use s3 to storage files

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,25 @@
-export default [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+export default ({ env }) => [
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", `${env("CDN_BASE_URL")}`],
+          "media-src": ["'self'", "data:", "blob:", `${env("CDN_BASE_URL")}`],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
--- config/plugins.ts
+++ config/plugins.ts
@@ -10,4 +10,21 @@ export default ({ env }) => ({
       ],
     },
   },
+  upload: {
+    config: {
+      provider: "strapi-provider-upload-aws-s3-advanced",
+      providerOptions: {
+        accessKeyId: env("AWS_ACCESS_KEY_ID"),
+        secretAccessKey: env("AWS_ACCESS_SECRET"),
+        region: env("AWS_REGION"),
+        endpoint: env("AWS_ENDPOINT"),
+        params: {
+          bucket: env("AWS_BUCKET"),
+          acl: env("AWS_BUCKET_ACL"),
+        },
+        baseUrl: env("CDN_BASE_URL"),
+        prefix: env("BUCKET_PREFIX"),
+      },
+    },
+  },
 });
